908957d5f7b0c0ee5d8fbde5ab68f1142d28b73c
Marcos Wasem <mymakecoins@gmail.com>
2026-08-10 15:49:22 -0300

    fix(db): add transaction boundaries to migration 000023 down sql

--- migrations/000023_v1_507_v1_508_v1_509_intelligence.down.sql
+++ migrations/000023_v1_507_v1_508_v1_509_intelligence.down.sql
@@ -1,6 +1,10 @@
+BEGIN;
+
 DROP TABLE IF EXISTS insight_feedbacks;
 DROP TABLE IF EXISTS insight_thresholds;
 DROP TABLE IF EXISTS operational_insights;
 DROP TABLE IF EXISTS briefing_preferences;
 DROP TABLE IF EXISTS briefing_records;
 DROP TABLE IF EXISTS change_records;
+
+COMMIT;
